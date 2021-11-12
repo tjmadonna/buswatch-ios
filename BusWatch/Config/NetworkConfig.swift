9360a7cb8e40d2da072903da7bbfcacac9ac1fe6
Tyler Madonna <developer@madonnaapps.com>
2021-11-11 20:37:44 -0500

    Added network config file

---
+++ BusWatch/Config/NetworkConfig.swift
@@ -0,0 +1,20 @@
+//
+//  RemoteConfig.swift
+//  BusWatch
+//
+//  Created by Tyler Madonna on 5/2/20.
+//  Copyright © 2020 Tyler Madonna. All rights reserved.
+//
+
+import Foundation
+
+enum NetworkConfig {
+
+    static let scheme = "https"
+
+    static let host = "truetime.portauthority.org"
+
+    static let apiKey = "YOUR API KEY HERE"
+
+    static let basePath = "/bustime/api/v3"
+}
