6fd39d38ce040d0f040e33939879a971128eb677
Tyler Madonna <developer@madonnaapps.com>
2022-01-06 16:53:40 -0500

    Switched tolazy vars in app component

--- BusWatch/Dependencies/AppComponent.swift
+++ BusWatch/Dependencies/AppComponent.swift
@@ -13,27 +13,21 @@ import BWTOverview
 
 final class AppComponent {
 
-    private var database: DatabaseDataSourceRepresentable?
+    private lazy var database: DatabaseDataSourceRepresentable = {
+        let userDefaultsDataSource = self.userDefaults
+        let populator = DatabasePopulator(userDefaultaDataSource: userDefaultsDataSource)
+        return DatabaseDataSource(databasePath: DatabaseConfig.url, databasePopulator: populator)
+    }()
 
-    private var userDefaults: UserDefaultsDataSourceRepresentable?
+    private lazy var userDefaults: UserDefaultsDataSourceRepresentable = {
+        return UserDefaultsDataSource()
+    }()
 
     func provideDatabaseDataSource() -> DatabaseDataSourceRepresentable {
-        if let database = database {
-            return database
-        }
-        let userDefaultsDataSource = provideUserDefaultsDataSource()
-        let populator = DatabasePopulator(userDefaultaDataSource: userDefaultsDataSource)
-        let database = DatabaseDataSource(databasePath: DatabaseConfig.url, databasePopulator: populator)
-        self.database = database
         return database
     }
 
     func provideUserDefaultsDataSource() -> UserDefaultsDataSourceRepresentable {
-        if let userDefaults = userDefaults {
-            return userDefaults
-        }
-        let userDefaults = UserDefaultsDataSource()
-        self.userDefaults = userDefaults
         return userDefaults
     }
 }
