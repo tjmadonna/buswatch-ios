bc81b2c77c8b32cbfdf8a1a4761530c91fbad9fb
Tyler Madonna <developer@madonnaapps.com>
2022-03-11 10:48:09 -0500

    Catch endpoint predictions error message

--- BusWatch/Features/Predictions/Network/DataSources/PredictionsPredictionDataSourceImpl.swift
+++ BusWatch/Features/Predictions/Network/DataSources/PredictionsPredictionDataSourceImpl.swift
@@ -11,6 +11,10 @@ import Combine
 
 final class PredictionsPredictionDataSourceImpl: PredictionsPredictionDataSource {
 
+    enum Error: Swift.Error {
+        case endpoint
+    }
+
     private let urlSource: UrlSource
 
     private let urlSession: URLSession
@@ -27,14 +31,19 @@ final class PredictionsPredictionDataSourceImpl: PredictionsPredictionDataSource
 
     // MARK: - PredictionsPredictionDataSource
 
-    func getPredictionsForStopId(_ stopId: String) -> AnyPublisher<[PredictionsDataPrediction], Error> {
+    func getPredictionsForStopId(_ stopId: String) -> AnyPublisher<[PredictionsDataPrediction], Swift.Error> {
 
         let url = urlSource.authenticatedPredictionsURLForStopId(stopId)
 
         return TimedNetworkPublisher<PredictionsNetworkGetPredictionsResponse>(url: url,
                                                                                timeInterval: 30,
                                                                                urlSession: self.urlSession)
-            .map { response in response.bustimeResponse?.predictions ?? [] }
+            .tryMap { response in
+                if response.bustimeResponse?.errors != nil {
+                    throw Error.endpoint
+                }
+                return response.bustimeResponse?.predictions ?? []
+            }
             .map { [weak self] predictions in
                 self?.mapper.mapNetworkPredictionArrayToDataPredictionArray(predictions) ?? []
             }
